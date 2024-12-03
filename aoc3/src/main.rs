5e74f727b3e3396eccb18ea98184b76afa28f2b1
Sietse Verbeek <sietse.verbeek@outlook.com>
2024-12-03 22:02:30 +0100

    Add day 3 part 2

--- aoc3/src/main.rs
+++ aoc3/src/main.rs
@@ -7,11 +7,27 @@ fn main() {
     let input = fs::read_to_string("input").unwrap();
     let reg = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
-    let total: i64 = reg.captures_iter(&input)
+    let total1: i64 = reg.captures_iter(&input)
         .map(|x| {
             let (_, groups): (&str, [&str; 2]) = x.extract();
             let nums: i64 = groups.iter().filter_map(|x| x.parse::<i64>().ok()).product();
             nums
         }).sum();
-    println!("{:?}", total);
+
+    let strings: Vec<&str> = input.split("do()")
+        .filter_map(|x| x.split("don't").next())
+        .collect();
+
+    let total2: i64 = strings.into_iter()
+        .map(|x| {
+            reg.captures_iter(&x)
+                .map(|x| {
+                    let (_, groups): (&str, [&str; 2]) = x.extract();
+                    let nums: i64 = groups.iter().filter_map(|x| x.parse::<i64>().ok()).product();
+                    nums
+                }).sum::<i64>()
+        }).sum();
+
+    println!("{:?}", total1);
+    println!("{:?}", total2);
 }
